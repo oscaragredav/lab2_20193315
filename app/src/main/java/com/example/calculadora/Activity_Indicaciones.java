d1778759dcb86cd6e48006b9292b3ad582b9883e
oscaragredav <oscar@itic.com.pe>
2024-04-05 13:35:59 -0500

    parte visual

---
+++ app/src/main/java/com/example/calculadora/Activity_Indicaciones.java
@@ -0,0 +1,26 @@
+package com.example.calculadora;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Activity_Indicaciones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_indicaciones);
+
+    }
+
+    public void regresarCalcular (View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -1,24 +1,27 @@
 package com.example.calculadora;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+    }
+
+    public  void abrirIndicaciones (View view){
+        Intent intent = new Intent(this, Activity_Indicaciones.class);
+        startActivity(intent);
+    }
+
+    public  void abrirCalculadora (View view){
+        Intent intent = new Intent(this, ActivityCalculadora.class);
+        startActivity(intent);
     }
 }
